34f77d9252d5dc4d2379fc9cedeb6643ef30092d
DESKTOP-7QBQJEG\yunis <luckchurch@gmail.com>
2021-09-30 19:24:51 +0900

    Javascript-Observer 를 이용한  HTML Section 추가하기

---
+++ pages/observer.tsx
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useState } from "react";
+
+import type { NextPage } from "next";
+import styles from "../styles/ObserverPage.module.css";
+
+const ObserverPage: NextPage = () =>{
+    const [list, setList] = useState(['1','2','3','4'])
+    let count = 0
+    useEffect(() => { 
+        const sections = document.getElementsByTagName('section');
+        count = sections.length;
+        const observer = new IntersectionObserver(handleObserver, options); 
+        if (sections) {
+            Array.from(sections).forEach(e=>{
+                observer.observe(e)
+            })
+            observer.observe(sections[1]); 
+        }
+        return () => observer.disconnect();
+    }, [list]);
+    
+    const handleObserver = useCallback(async (entries) => { 
+        const entriesT = entries[0]; 
+        if (entriesT.isIntersecting) { 
+            console.log(entriesT.target.innerHTML)
+            // console.log("is InterSecting"); 
+        } 
+        if (entriesT.intersectionRatio > 0) { 
+            // console.log("intersectionRatio"); 
+        } 
+    }, []);
+    const options = { 
+        root: null,  //기본 null, 관찰대상의 부모요소를 지정 
+        rootMargin: "20px", // 관찰하는 뷰포트의 마진 지정 
+        threshold: 1.0, // 관찰요소와 얼만큼 겹쳤을 때 콜백을 수행하도록 지정하는 요소 
+    };
+
+    const addClick = useCallback(
+        () => {
+            count ++;
+            setList([...list, String(count)])
+        },
+        [list],
+    )
+
+
+    return (
+        <>
+            <button onClick={addClick} className={styles.add_button__fixed}>ADD</button>
+            {list.map((e, index)=>{
+                return (
+                    <section key={index} className={styles.container_section}>
+                        {e}
+                    </section>
+                );
+
+            })}
+        </>
+    )
+}
+
+export default ObserverPage
