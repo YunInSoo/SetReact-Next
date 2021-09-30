0b9046e5cab178d7531bf0eed2ae90f734b66698
DESKTOP-7QBQJEG\yunis <luckchurch@gmail.com>
2021-09-30 17:40:26 +0900

    Context API and Redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,65 @@
+import { InitState, changeNameAction } from '../reducers/redux';
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
 import Image from 'next/image'
 import type { NextPage } from 'next'
+import UserContext from '../constants/UserContext';
+import { changeNameActionContext } from '../reducers/contextAPI';
 import styles from '../styles/Home.module.css'
+import useInput from "../hooks/useInput";
 
 const Home: NextPage = () => {
+  const context: any = useContext(UserContext);
+  
+  //Redux========================================================
+  const dispatch = useDispatch();
+  const nameInput = useInput('')
+  const state = useSelector((state: InitState) => state)
+  const formSubmit = useCallback((e)=>{
+    dispatch(changeNameAction(nameInput.value));
+    e.preventDefault()
+  },[nameInput.value]);
+  
+  const nameUserData = useMemo(() => { 
+    let name = state.name;
+    name = name.split(" ")[0]
+    return name;
+  }, [state.name])
+  
+  //ContextAPI===================================================
+  const {user, dispatchUser} = context;
+  const nameInputTwo = useInput('')
+  const formSubmitTwo = useCallback((e)=>{
+    dispatchUser({type:'CHANGE_NICKNAME', data:nameInputTwo.value});
+    console.log(user.name)
+    e.preventDefault()
+  },[nameInputTwo.value]);
+
+  const nameUserDataTwo = useMemo(() => { 
+    let name = user.name;
+    name = name.split(" ")[0]
+    return name;
+  }, [user])
+  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {/* Redux */}
+        <div>Redux</div>
+        <form onSubmit={formSubmit}>
+          <input type="text" placeholder="입력하세요" {...nameInput} />
+          <button type="submit">{nameUserData}</button>
+        </form>
+        <div>Context API</div>
+        {/* ContextAPI */}
+        <form onSubmit={formSubmitTwo}>
+          <input type="text" placeholder="입력하세요" {...nameInputTwo} />
+          <button type="submit">{nameUserDataTwo}</button>
+        </form>
       </main>
 
       <footer className={styles.footer}>
