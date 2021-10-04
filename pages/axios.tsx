1c17afedcf717c2d02d0c748ea552720dd0c4c0d
DESKTOP-7QBQJEG\yunis <luckchurch@gmail.com>
2021-10-05 00:26:48 +0900

    immer libary를 이용한 불변성 관리

--- pages/axios.tsx
+++ pages/axios.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import { NextPage } from "next";
+import produce from "immer"
 import useAxios from "../hooks/useAxios";
 import useInput from "../hooks/useInput";
 
@@ -14,7 +15,8 @@ export type listModels = {
     title: string,
 }
 const AxiosPage: NextPage = () => {
-    let emailTemp, contextTemp;
+    let emailTemp: string;
+    let contextTemp: string;
 
 
 
@@ -30,6 +32,7 @@ const AxiosPage: NextPage = () => {
         return axiosList;
     }, [axiosData])
 
+    //기본 spread연산자를 이용해 불변성 지키기
     const onSubmitAxios = useCallback(
         (e) => {
             contextTemp = context.value;
@@ -51,15 +54,44 @@ const AxiosPage: NextPage = () => {
         },
         [axiosData, context, email],
     )
+
+    //불변성을 쉽게 관리할수있는 immer 라이브러리
+    const onSubmitAxios2 = useCallback(
+        (e) => {
+            contextTemp = context.value;
+            emailTemp = email.value;
+            setAxiosData(
+                produce((draftState: any) => {
+                    draftState.data.data.data.movies.push({
+                        title: contextTemp
+                    })
+                })
+            )
+            e.preventDefault()
+        },
+        [axiosData, context, email],
+    )
     return (
         <>
             <h1>AXIOS</h1>
             <div>Test form</div>
+
+            <div>spread를 이용한 불변성관리</div>
+
             <form onSubmit={onSubmitAxios}>
                 <input {...context} type="text" name="context" />
                 <input {...email} type="email" name="email" />
                 <button type="submit">a</button>
             </form>
+
+            <div>immer libary를 이용한 불변성 관리</div>
+
+            <form onSubmit={onSubmitAxios2}>
+                <input {...context} type="text" name="context" />
+                <input {...email} type="email" name="email" />
+                <button type="submit">a</button>
+            </form>
+
             <ul>{list?.map((e, index) => {
                 return (
                     <li key={index}>{e.title}</li>
